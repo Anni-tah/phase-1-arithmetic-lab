b24c0673999437cf727c951da8f249d561fef107
Anne Nabwire <nabanne96@gmail.com>
2024-12-09 10:13:52 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//question 1
+let num1 =31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+//question 2
+let random =Math.ceil (Math.random()*10);
+
+//question 3
+let num3 =14;
+let num4 =5;
+let mod =num3 % num4;
+console.log(mod);
+
+//question 4
+ let max = Math.max(1,10,17, 16,20,14,7);
+document.write(max);
